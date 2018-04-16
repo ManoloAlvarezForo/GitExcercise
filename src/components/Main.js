0103452d24009ee99ec521a11fa487c9d019a01f
Manolo Alvarez Foro <af.manolo@gmail.com>
2018-04-16 07:16:19 -0400

    routes: Added the initial routes with react-router

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Main = () => {
+    return(
+        <div>
+            <div style={{ display:'flex', flexDirection:'row', justifyContent:'center'}}>
+            <div style={{ display:'flex', flexDirection:'column', justifyContent:'center'}}>
+               <h1>Github Users Excercise</h1>
+               <div style={{ display:'flex', flexDirection:'row', justifyContent:'center'}}>
+                 <button>Gihub Users</button>
+               </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
